5427448217e1fddcf777babafac9cd1b2bba7783
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2024-12-18 22:26:51 -0300

    feat: adding the importation

--- download/__init__.py
+++ download/__init__.py
@@ -3,3 +3,4 @@ from .PortalConvenioDownloader import PortalConvenioDownloader
 from .OrcamentoDownloader import OrcamentoDownloader
 from .SiconvDownloader import SiconvDownloader
 from .PainelObras import PainelObras
+from .PainelParlamentar import PainelParlamentar
