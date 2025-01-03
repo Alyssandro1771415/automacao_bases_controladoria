b0e38d41aa28b0ad1a99feb9594f9297c49e54c1
Luccas Jose Bezerra Fernandes <luccas.fernandes@scge.local>
2025-01-03 14:12:31 -0300

    refactor: atualização da classe pai e da classe relativa ao OGU - implementação de multiplos navegadores.

--- download/BaseDownloader.py
+++ download/BaseDownloader.py
@@ -3,6 +3,17 @@ import os
 import logging
 from functools import wraps
 import time
+import glob
+import re
+import zipfile
+import shutil
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
 class BaseDownloader(ABC):
     
@@ -29,25 +40,123 @@ class BaseDownloader(ABC):
     def retry(max_attempts=3, delay=60):
         def decorator(func):
             @wraps(func)
-            def wrapper(self, *args, **kwargs):
-                attempts = 0
-                while attempts < max_attempts:
-                    try:
-                        return func(self, *args, **kwargs)
-                    except Exception as e:
-                        attempts += 1
-                        if attempts == max_attempts:
-                            self.log_error(f'Todas as {max_attempts} tentativas falharam. Erro final: {str(e)}')
-                            raise
-                        self.logger.warning(f'Tentativa {attempts} falhou. Realizando outra tentativa em {delay} segundos..')
-                        time.sleep(delay)
+            def wrapper(self, *args, browser="firefox", **kwargs):
+                browsers = ["firefox", "edge", "chrome"]
+                for current_browser in browsers:
+                    attempts = 0
+                    while attempts < max_attempts:
+                        try:
+                            return func(self, *args, browser=current_browser, **kwargs)
+                        except Exception as e:
+                            attempts += 1
+                            error_message = f'Tentativa {attempts} com {current_browser} falhou. Erro: {type(e).__name__}: {str(e)}'
+                            self.log_error(error_message)
+                            if attempts == max_attempts:
+                                if current_browser != browsers[-1]:
+                                    self.logger.info(f"Todas as tentativas com {current_browser} falharam. Tentando com o próximo navegador...")
+                                    break
+                                else:
+                                    final_error = f'Todas as tentativas com todos os navegadores falharam.'
+                                    self.log_error(final_error)
+                                    raise
+                            self.logger.info(f'Tentando novamente com {current_browser} em {delay} segundos...')
+                            time.sleep(delay)
             return wrapper
         return decorator
     
+    def get_driver(self, browser):
+        if browser == "firefox":
+            options = webdriver.FirefoxOptions()
+            options.set_preference("browser.download.folderList", 2)
+            options.set_preference("browser.download.dir", self.download_dir)
+            options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/zip")
+            options.set_preference("pdfjs.disabled", True)
+            return webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+        elif browser == "edge":
+            options = webdriver.EdgeOptions()
+            options.add_experimental_option("prefs", {
+                "download.default_directory": self.download_dir,
+                "download.prompt_for_download": False,
+                "download.directory_upgrade": True,
+                "safebrowsing.enabled": True
+            })
+            return webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
+        elif browser == "chrome":
+            options = webdriver.ChromeOptions()
+            options.add_experimental_option("prefs", {
+                "download.default_directory": self.download_dir,
+                "download.prompt_for_download": False,
+                "download.directory_upgrade": True,
+                "safebrowsing.enabled": True
+            })
+            return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        else:
+            raise ValueError("Navegador não suportado")
+
     @abstractmethod
-    def download(self):
+    def download(self, browser="firefox"):
         pass
         
     def setup_directories(self):
+        if not os.path.exists(self.download_dir):
+            os.makedirs(self.download_dir)
         if not os.path.exists(self.final_dir):
             os.makedirs(self.final_dir)
+
+    def wait_for_download(self, zip_file_name, max_wait_time=600):
+        start_time = time.time()
+
+        while time.time() - start_time < max_wait_time:
+            zip_files = glob.glob(os.path.join(self.download_dir, "*.zip"))
+            matching_files = [file for file in zip_files if re.match(zip_file_name, os.path.basename(file))]
+            
+            if matching_files:
+                zip_path = matching_files[0]
+                if not any(file.endswith('.part') or file.endswith('.crdownload') for file in os.listdir(self.download_dir)):
+                    self.logger.info('Download concluído...')
+                    return zip_path
+            
+            self.logger.info('Aguardando o download do arquivo zip...')
+            time.sleep(15)
+
+        error_message = "Timeout: O download não foi concluído no tempo esperado."
+        self.log_error(error_message)
+        raise TimeoutError(error_message)
+
+    def extract_and_cleanup(self, zip_path, rename_to, delete_pattern, rename_pattern):
+        try:
+            shutil.move(zip_path, self.final_dir)
+            self.logger.info(f'Arquivo movido para a pasta: {self.final_dir}')
+            moved_file_path = os.path.join(self.final_dir, os.path.basename(zip_path))
+            
+            with zipfile.ZipFile(moved_file_path, 'r') as zip_ref:
+                self.logger.info('Extraindo o zip...')
+                zip_ref.extractall(self.final_dir)
+                        
+            self.logger.info('Deletando .zip')
+            os.remove(moved_file_path)
+        
+            files = zip_ref.namelist()
+            file_to_delete = next((file for file in files if delete_pattern and re.match(delete_pattern, os.path.basename(file))), None)
+            file_to_rename = next((file for file in files if rename_pattern and re.match(rename_pattern, os.path.basename(file))), None)        
+
+            self.logger.info(f"Arquivos encontrados - Para deletar: {file_to_delete}, Para renomear: {file_to_rename}")
+        
+            if file_to_delete:
+                os.remove(os.path.join(self.final_dir, file_to_delete))
+                self.logger.info(f'Arquivo "{file_to_delete}" deletado com sucesso!')
+        
+            if file_to_rename and rename_to:
+                os.rename(os.path.join(self.final_dir, file_to_rename), os.path.join(self.final_dir, rename_to))
+                self.logger.info(f'Arquivo "{file_to_rename}" renomeado com sucesso para "{rename_to}"!')
+
+        except Exception as e:
+            error_message = f"Erro durante a extração e limpeza: {type(e).__name__}: {str(e)}"
+            self.log_error(error_message)
+            raise
+
+    def check_directory_permissions(self, directory):
+        if not os.access(directory, os.W_OK):
+            error_message = f"Sem permissão de escrita no diretório: {directory}"
+            self.log_error(error_message)
+            raise PermissionError(error_message)
--- download/OrcamentoDownloader.py
+++ download/OrcamentoDownloader.py
@@ -1,4 +1,3 @@
-
 import os
 import time
 import zipfile
@@ -6,8 +5,9 @@ import shutil
 import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from .BaseDownloader import BaseDownloader
 
 class OrcamentoDownloader(BaseDownloader):
@@ -15,50 +15,70 @@ class OrcamentoDownloader(BaseDownloader):
     def __init__(self, download_dir, final_dir):
         super().__init__(download_dir, final_dir)
 
-    def download(self):
+    def find_most_recent_download_link(self, driver):
+        wait = WebDriverWait(driver, 10)
+        try:
+            download_links = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[contains(@href, 'BD_Gestores_')]")))
+        except TimeoutException:
+            self.logger.info('Não foi possível encontrar links de download.')
+            return None, None
+
+        latest_date = datetime.date(1900, 1, 1)
+        latest_link = None
+        for link in download_links:
+            href = link.get_attribute('href')
+            date_str = href.split('BD_Gestores_')[1].split('.zip')[0]
+            date = datetime.datetime.strptime(date_str, '%d_%m_%Y').date()
+            if date > latest_date:
+                latest_date = date
+                latest_link = link
+
+        return latest_link, latest_date
+
+    @BaseDownloader.retry(max_attempts=3, delay=60)
+    def download(self, browser="firefox"):
         self.setup_directories()
-        self.logger.info('Iniciando o processo de download e busca - OGU..')
+        self.logger.info(f'Iniciando o processo de download e busca - OGU com {browser}..')
 
-        options = webdriver.FirefoxOptions()
-        options.set_preference("browser.download.folderList", 2)
-        options.set_preference("browser.download.dir", self.download_dir)
-        options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/zip")
-        options.set_preference("pdfjs.disabled", True)
-
-        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
-        driver.get("https://www.caixa.gov.br/site/paginas/downloads.aspx")
-        time.sleep(5)
-
-        zip_file_name = f"BD_Gestores_{datetime.date.today().strftime('%d_%m_%Y')}.zip"
-        zip_path = os.path.join(self.download_dir, zip_file_name)
+        driver = self.get_driver(browser)
+        wait = WebDriverWait(driver, 10)
 
         try:
-            download_button = driver.find_element(By.XPATH, "//button[contains(@class, 'botao-categoria') and @data-categoria='944']")
-            download_button.click()
-            time.sleep(3)
+            driver.get("https://www.caixa.gov.br/site/paginas/downloads.aspx")
+            self.logger.info('Página de download acessada..')
 
-            download_link = driver.find_element(By.XPATH, f"//a[contains(@href, '{zip_file_name}')]")
-            download_link.click()
-            print("Download iniciado...")
-            
-            while True:
-                if os.path.exists(zip_path) and not any(file.endswith('.part') or file.endswith('.crdownload') for file in os.listdir(self.download_dir)):
-                    print("Download concluído!")
-                    break
-                else:
-                    
-                    print("Aguardando o download do arquivo zip...")
-                    time.sleep(15)
-        
+            download_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'botao-categoria') and @data-categoria='944']")))
+            download_button.click()
+            self.logger.info('Botão de download clicado...')
+
+            latest_link, latest_date = self.find_most_recent_download_link(driver)
+            if latest_link:
+                zip_file_name = f"BD_Gestores_{latest_date.strftime('%d_%m_%Y')}.zip"
+                zip_path = os.path.join(self.download_dir, zip_file_name)
+                latest_link.click()
+                self.logger.info(f'Iniciando download de {zip_file_name}...')
+            else:
+                self.log_error('Não foi possível encontrar um link de download válido.')
+                return
+
+            # Espera pelo download do arquivo
+            self.wait_for_download(zip_file_name)
+            self.logger.info('Download concluído...')
+
+            if os.path.exists(zip_path):
+                self.extract_and_cleanup(zip_path, None, None, None)
+            else:
+                self.log_error(f'Arquivo zip não encontrado: {zip_path}')
+
+        except TimeoutException as e:
+            self.log_error(f'Timeout ao esperar por um elemento: {str(e)}')
+            raise
+        except NoSuchElementException as e:
+            self.log_error(f'Elemento não encontrado: {str(e)}')
+            raise
+        except Exception as e:
+            self.log_error(f'Erro inesperado durante o download: {str(e)}')
+            raise
         finally:
             driver.quit()
-
-        if os.path.exists(zip_path):
-            shutil.move(zip_path, self.final_dir)
-            moved_file_path = os.path.join(self.final_dir, zip_file_name)
-            
-            with zipfile.ZipFile(moved_file_path, 'r') as zip_ref:
-                zip_ref.extractall(self.final_dir)
-                
-            os.remove(moved_file_path)
-            print("Arquivo de Orçamento Geral da União extraido e removido com sucesso!")
+            self.logger.info('Navegador fechado...')
